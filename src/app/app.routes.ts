ee472a3d07e7d642bcd1019ccf3a633e72bd2965
Laura <laura.diaz.reguera.alu@iesfernandoaguilar.es>
2026-03-18 00:52:05 +0100

    Commit inicial: Proyecto inicial Angular (Navbar creado)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { SkillsComponent } from './components/skills/skills';
+import { ProjectsComponent } from './components/projects/projects';
+import { ContactComponent } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // La ruta por defecto es el home
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Si escriben una ruta que no existe, vuelve al Home
+];
