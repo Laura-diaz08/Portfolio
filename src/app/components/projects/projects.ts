ebcc3d8a1e1f5653e193ad883773f2039e833546
Laura <laura.diaz.reguera.alu@iesfernandoaguilar.es>
2026-03-18 01:29:20 +0100

    Pagina de Proyectos creada

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   imports: [],
   templateUrl: './projects.html',
-  styleUrl: './projects.css',
+  styleUrl: './projects.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: any[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getPortfolioData().subscribe(data => {
+      this.projects = data.projects;
+    });
+  }
 }
