bff7f960d55e0f36d67533f5211ee1c65cd9727a
Laura <laura.diaz.reguera.alu@iesfernandoaguilar.es>
2026-03-18 12:53:23 +0100

    Estilo modificado de las paginas Home y Contact

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class ContactComponent {
-  
+
+  contactForm: FormGroup;
+
+  enviado = false;
+  error = false;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mensaje: new FormControl('', [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  onEnviar() {
+    if (this.contactForm.valid) {
+
+      console.log('Datos del formulario:', this.contactForm.value);
+      
+      this.enviado = true;
+      this.error = false;
+      this.contactForm.reset(); 
+      
+      setTimeout(() => this.enviado = false, 5000);
+
+    } else {
+      this.error = true;
+      setTimeout(() => this.error = false, 4000);
+    }
+  }
 }
