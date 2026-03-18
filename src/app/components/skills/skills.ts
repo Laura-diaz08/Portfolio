7cbe654befe9d3347add7a4a6498951757f60044
Laura <laura.diaz.reguera.alu@iesfernandoaguilar.es>
2026-03-18 01:25:08 +0100

    Página de habilidades creada

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data';
 
 @Component({
   selector: 'app-skills',
+  standalone: true,
   imports: [],
   templateUrl: './skills.html',
-  styleUrl: './skills.css',
+  styleUrl: './skills.css'
 })
-export class SkillsComponent{
+export class SkillsComponent implements OnInit {
+  skills: any[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getPortfolioData().subscribe(data => {
+      this.skills = data.skills;
+    });
+  }
 }
